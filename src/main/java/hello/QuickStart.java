54381616039927c2a34942a5c9df514bdf9c6c92
Mike McCarthy <mike.mccarthy@gamesys.co.uk>
2019-02-15 17:30:38 +0000

    Initial commit of POC code

---
+++ src/main/java/hello/QuickStart.java
@@ -0,0 +1,174 @@
+package hello;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+
+import com.google.api.services.youtube.YouTubeScopes;
+import com.google.api.services.youtube.model.*;
+import com.google.api.services.youtube.YouTube;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class QuickStart {
+
+    /** Application name. */
+    private static final String APPLICATION_NAME = "API Sample";
+
+    /** Directory to store user credentials for this application. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+            System.getProperty("user.home"), ".credentials/youtube-java-quickstart");
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY =
+            JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
+
+    /** Global instance of the scopes required by this quickstart.
+     *
+     * If modifying these scopes, delete your previously saved credentials
+     * at ~/.credentials/drive-java-quickstart
+     */
+    private static final List<String> SCOPES =
+            Arrays.asList(YouTubeScopes.YOUTUBE_READONLY);
+
+    private static final long NUMBER_OF_VIDEOS_RETURNED = 25;
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Create an authorized Credential object.
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    public static Credential authorize() throws IOException {
+        // Load client secrets.
+        InputStream in =
+                QuickStart.class.getResourceAsStream("/client_secret.json");
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                        .setDataStoreFactory(DATA_STORE_FACTORY)
+                        .setAccessType("offline")
+                        .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+                flow, new LocalServerReceiver()).authorize("user");
+        return credential;
+    }
+
+    /**
+     * Build and return an authorized API client service, such as a YouTube
+     * Data API client service.
+     * @return an authorized API client service
+     * @throws IOException
+     */
+    public static YouTube getYouTubeService() throws IOException {
+        Credential credential = authorize();
+        return new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+            YouTube youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+                public void initialize(HttpRequest request) throws IOException {
+                }
+            }).setApplicationName("SearchFilter").build();
+
+            String apiKey = "AIzaSyBXJ6FxA27Z9NDMMx9mcekU6DyTrhw4Ms0";
+            YouTube.Search.List search = youtube.search().list("id,snippet");
+
+            String queryTerm = "news";
+
+            search.setKey(apiKey);
+            search.setQ(queryTerm);
+            search.setType("video");
+
+//            search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+            search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+
+            SearchListResponse searchResponse = search.execute();
+            List<SearchResult> searchResultList = searchResponse.getItems();
+
+            if (searchResultList != null) {
+                prettyPrint(searchResultList.iterator(), queryTerm);
+            }
+
+        } catch (GoogleJsonResponseException e) {
+            e.printStackTrace();
+            System.err.println("There was a service error: " +
+                    e.getDetails().getCode() + " : " + e.getDetails().getMessage());
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    private static void prettyPrint(Iterator<SearchResult> iteratorSearchResults, String query) {
+
+        System.out.println("\n=============================================================");
+        System.out.println(
+                "   First " + NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + query + "\".");
+        System.out.println("=============================================================\n");
+
+        if (!iteratorSearchResults.hasNext()) {
+            System.out.println(" There aren't any results for your query.");
+        }
+
+        while (iteratorSearchResults.hasNext()) {
+
+            SearchResult singleVideo = iteratorSearchResults.next();
+            if (!singleVideo.getSnippet().getChannelId().equals("UC16niRr50-MSBwiO3YDb3RA")) {
+
+                ResourceId rId = singleVideo.getId();
+
+                // Confirm that the result represents a video. Otherwise, the
+                // item will not contain a video ID.
+                if (rId.getKind().equals("youtube#video")) {
+                    Thumbnail thumbnail = singleVideo.getSnippet().getThumbnails().getDefault();
+
+                    System.out.println(" Video Id: " + rId.getVideoId());
+                    System.out.println(" Title: " + singleVideo.getSnippet().getTitle());
+                    System.out.println(" Channel Title: " + singleVideo.getSnippet().getChannelTitle());
+                    System.out.println(" Channel ID: " + singleVideo.getSnippet().getChannelId());
+                    System.out.println(" Thumbnail: " + thumbnail.getUrl());
+                    System.out.println("\n-------------------------------------------------------------\n");
+                }
+            }
+        }
+    }
+}
